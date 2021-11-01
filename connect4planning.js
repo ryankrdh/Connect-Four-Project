dc4dc61cc27859b779a7e8ff929a25b4bf4914ea
Ryan Kim <ryankrdh@gmail.com>
2021-10-31 21:28:08 -0700

    starting connect four project

---
+++ connect4planning.js
@@ -0,0 +1,10 @@
+// we need a 7 wide 6 deep grid with one horizonal line at top for dropping the dots.
+// a played piece will be represented as a dot (red or blue)
+
+// functions
+/*
+gameBoard
+playerOne
+playerTwo
+
+*/
